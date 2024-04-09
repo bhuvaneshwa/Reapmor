a7c1f0cdb53d891f98fe39f85f3bdc415f2f6970
bhuvaneshwa <bhuvaneshwaran368@gmail.com>
2024-04-09 12:36:35 +0530

    09-DAY-3

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -10,34 +10,38 @@ export default function Hero() {
     }, 2000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [texts.length]);
 
   return (
     <div>
       <div
-        className="relative h-screen bg-cover bg-center"
+        className="relative h-screen bg-cover bg-fixed bg-center"
         style={{
-          backgroundImage: 'url("/bg.webp")',
+          backgroundImage: 'url("/home-image.jpg")',
         }}
       >
-        <div className="absolute sm:mx-0 xl:mx-14 sm:w-full m-24 top-1/4 left-1/5 transform -translate-y-1/2 text-left px-8 font-sans">
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <div>
-              <h1 className="text-4xl lg:text-[60px] text-white font-light tracking-wide leading-normal mb-8">
+        {/* Dark overlay */}
+        <div className="absolute inset-0 bg-black opacity-65"></div>
+
+        <div className="absolute sm:mx-0 xl:mx-14 sm:w-full w-4/5 m-24 top-1/4 left-1/10 transform -translate-y-1/2 text-left px-8">
+          <div className="grid grid-cols-1 lg:grid-cols-1 gap-8">
+            <div className="w-full">
+              <h1 className="text-4xl lg:text-[60px] text-white font-extralight tracking-wide leading-normal mb-8">
                 <span className="">A digital identity</span> <br />
                 <span className="mr-2 pt-4">for every</span> <br />
-                <span className="mr-2 text-[#c2fc03] lg:text-6xl xl:text-[60px]">
-                  {texts[textIndex]}
-                </span>
+                <span className="mr-2 text-[#c2fc03] lg:text-6xl xl:text-[60px]">{texts[textIndex]}</span>
               </h1>
-              <p className="text-[17px] font-light text-justify text-white mb-8">
-                Introducing {"India's"} first digital agriculture directory, reimagining the industry with data-driven solutions for crop planning, procurement, and targeted marketing.
+            
+
+              <p className="text-[18px]  tracking-wider  leading-8  font-extralight text-justify text-white mb-8">
+                Introducing India’s first digital agriculture directory, reimagining the industry with <br/>innovative data-driven solutions for 
+                 procurement, crop planning and targeted marketing.
               </p>
-              <button className="px-6 py-3 bg-white text-black rounded-full font-light hover:text-white hover:bg-green-800 transition duration-300 ease-in-out">
-                View Coverage
-              </button>
+
+            
+             
             </div>
-            {/* You can add another grid item for additional content */}
+           
           </div>
         </div>
       </div>
